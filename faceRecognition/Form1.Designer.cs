d23d1b639d7f37cb24a3b92401d71536e8e9cbef
Juan Pablo <juancho27392@hotmail.com>
2022-05-28 12:13:45 -0500

    Agregar archivos de proyecto.

---
+++ faceRecognition/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace faceRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tomar Foto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 127);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(315, 134);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre de Usuario:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Procesar Imagenes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(161, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reconocimiento";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 269);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Reconociento de Rostro";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ faceRecognition/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace faceRecognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ultimoId = "";
+        int id = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Process.Start("CMD.exe", "python c:/face-recognition/1dataset.py");
+
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Juan Pablo\source\repos\faceRecognition\faceRecognition\bin\Debug\dataset");
+
+            foreach (FileInfo file in dir.GetFiles())
+
+            {
+                ultimoId = file.Name;
+            }
+            try
+            {
+                id = Convert.ToInt32(ultimoId.Substring(8, 1)) + 1;
+            }
+            catch
+            {
+                id = 1;
+            }
+
+            //File.WriteAllText(@"C:\Users\IA\source\repos\faceRecognition\faceRecognition\bin\Debug\Usuarios.py", textBox1.Text);
+
+            string usuarios = File.ReadAllText(@"C:\Users\Juan Pablo\source\repos\faceRecognition\faceRecognition\bin\Debug\Usuarios.txt");
+            string[] lines = {
+                "import cv2",
+"import os",
+" ",
+"cam = cv2.VideoCapture(0)",
+"cam.set(3, 640) ",
+"cam.set(4, 480) ",
+" ",
+"face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')",
+" ",
+"face_id = "+id,
+" ",
+" ",
+"count = 0",
+" ",
+"while(True):",
+" ",
+"    ret, img = cam.read()",
+"    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)",
+"    faces = face_detector.detectMultiScale(gray, 1.3, 5)",
+" ",
+"    for (x,y,w,h) in faces:",
+" ",
+"        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     ",
+"        count += 1",
+" ",
+"        cv2.imwrite('dataset/Usuario.' + str(face_id) + '.' + str(count) + '.jpg', gray[y:y+h,x:x+w])",
+" ",
+"        cv2.imshow('image', img)",
+" ",
+"    k = cv2.waitKey(100) & 0xff ",
+"    if k == 27:",
+"        break",
+"    elif count >= 30: ",
+"         break",
+" ",
+"cam.release()",
+"cv2.destroyAllWindows()"
+            };
+            System.IO.File.WriteAllLines(@"C:\Users\Juan Pablo\source\repos\faceRecognition\faceRecognition\bin\Debug\1dataset.py", lines);
+
+            StreamWriter WriteReportFile = File.AppendText(@"C:\Users\Juan Pablo\source\repos\faceRecognition\faceRecognition\bin\Debug\Usuarios.txt");
+            WriteReportFile.WriteLine(",'" + textBox1.Text+"'");
+            WriteReportFile.Close();
+
+            string usuariosRec = File.ReadAllText(@"C:\Users\Juan Pablo\source\repos\faceRecognition\faceRecognition\bin\Debug\Usuarios.txt");
+
+            string[] lines2 = {
+            "import cv2",
+"import numpy as np",
+"import os ",
+" ",
+"recognizer = cv2.face.LBPHFaceRecognizer_create()",
+"recognizer.read('trainer/trainer.yml')",
+"cascadePath = "+"\""+"haarcascade_frontalface_default.xml"+"\"",
+"faceCascade = cv2.CascadeClassifier(cascadePath);",
+" ",
+"font = cv2.FONT_HERSHEY_SIMPLEX",
+" ",
+"id = " +id,
+" ",
+"names = [''"+usuariosRec+"]",
+" ",
+"cam = cv2.VideoCapture(0)",
+"cam.set(3, 640)",
+"cam.set(4, 480)",
+" ",
+"minW = 0.1*cam.get(3)",
+"minH = 0.1*cam.get(4)",
+" ",
+"while True:",
+" ",
+"    ret, img =cam.read()",
+" ",
+"    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)",
+" ",
+"    faces = faceCascade.detectMultiScale( ",
+"        gray,",
+"        scaleFactor = 1.2,",
+"        minNeighbors = 5,",
+"        minSize = (int(minW), int(minH)),",
+"       )",
+" ",
+"    for(x,y,w,h) in faces:",
+" ",
+"        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)",
+" ",
+"        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])",
+" ",
+"        if (confidence < 100):",
+"            id = names[id]",
+"            confidence = '  {0}%'.format(round(100 - confidence))",
+"        else:",
+"            id = 'unknown'",
+"            confidence = '  {0}%'.format(round(100 - confidence))",
+"         ",
+"        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)",
+"        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  ",
+"    ",
+"    cv2.imshow('camera',img) ",
+" ",
+"    k = cv2.waitKey(10) & 0xff",
+"    if k == 27:",
+"        break",
+" ",
+"cam.release()",
+"cv2.destroyAllWindows()",
+
+        }
+            ;
+            System.IO.File.WriteAllLines(@"C:\Users\Juan Pablo\source\repos\faceRecognition\faceRecognition\bin\Debug\3recognition.py", lines2);
+
+            sleepClick1();
+        }
+
+        private async Task sleepClick1()
+        {
+            updateStatusExecution("-> Iniciando captura de rostro. Mira la cámara y espera...");
+            await Task.Delay(1000);
+            executeCommand("python 1dataset.py");
+            updateStatusExecution("   Captura OK!");
+        }
+
+        private void executeCommand(String commandR)
+        {
+            try
+            {
+                var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = "/c " + commandR,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                //updateStatusExecution("*** TOMAR IMAGENES DEL USUARIO ***");
+                proc.Start();
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    string line = proc.StandardOutput.ReadLine();
+                    updateStatusExecution(line);
+                }
+            }
+            catch (Exception e)
+            {
+                updateStatusExecution("***Error while executing '" + commandR + "'");
+                updateStatusExecution("***Exception: '" + e.ToString());
+                updateStatusExecution("***Stack Trace: '" + e.StackTrace.ToString());
+            }
+        }
+        private void updateStatusExecution(String textR)
+        {
+            String currentDateTime = DateTime.Now.ToString(); ;
+            listBox1.Items.Add(textR);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            sleepClick2();
+        }
+        private async Task sleepClick2()
+        {
+            updateStatusExecution("-> Procesamiento de rostros. Tomará unos segundos. Espere...");
+            await Task.Delay(1000);
+            executeCommand("python 2training.py");
+            updateStatusExecution("   Procesamiento OK!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updateStatusExecution("-> Reconocimiento de rostro. Mire la cámara y espera...");
+            executeCommand("python 3recognition.py");
+            updateStatusExecution("   Reconocimiento OK!");
+        }
+    }
+}
